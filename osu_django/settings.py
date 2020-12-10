297a83996e979f7cb2f5bc0b6763b2b939da96aa
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2020-12-10 17:07:19 +0700

    Added proper migratiins, but forgot to commit. [stupid]

--- osu_django/settings.py
+++ osu_django/settings.py
@@ -59,7 +59,7 @@ JWT_AUTH = {
 
     'JWT_VERIFY': True,
     'JWT_VERIFY_EXPIRATION': True,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=3),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=3600),
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
 
 }
